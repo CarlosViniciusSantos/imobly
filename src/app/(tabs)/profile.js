aa0b9dd45bb4cbca87c93ce803e0c88c9aecfbb2
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-11-13 21:10:35 -0300

    atualizar quase funcionando, os dados já estão vindo

--- src/app/(tabs)/profile.js
+++ src/app/(tabs)/profile.js
@@ -1,17 +1,87 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView, Alert } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 import { TextInputMask } from 'react-native-masked-text';
 import Button from '../../components/ButtonDetails';
-import NavbarPadrao from '../../components/NavbarPadrao.js'
+import NavbarPadrao from '../../components/NavbarPadrao.js';
+import { useRouter } from 'expo-router';
+import { useLoginStore } from '../../stores/useLoginStore';
+import { getObjectData, storeObjectData } from '../../utils/asyncStorage';
 
 export default function AtualizarDadosUser() {
+    const { user, login } = useLoginStore();
+    const router = useRouter();
+
+    const [name, setName] = useState('');
+    const [cpf, setCpf] = useState('');
+    // const [nascimento, setNascimento] = useState('');
+    const [email, setEmail] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [estado, setEstado] = useState('');
+    const [telefone, setTelefone] = useState('');
+
+    useEffect(() => {
+        const loadUserData = async () => {
+            const storedUser = await getObjectData('userLogged');
+            if (storedUser) {
+                setName(storedUser.nome);
+                setCpf(storedUser.cpf);
+                // setNascimento(storedUser.nascimento);
+                setEmail(storedUser.email);
+                setCidade(storedUser.cidade);
+                setEstado(storedUser.estado);
+                setTelefone(storedUser.telefone);
+            }
+        };
+
+        loadUserData();
+    }, []);
+
+    const handleUpdate = async () => {
+        const userData = {
+            nome: name,
+            cpf,
+            // nascimento,
+            email,
+            cidade,
+            estado,
+            telefone,
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3000/users/${user.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${user.accessToken}`,
+                },
+                body: JSON.stringify(userData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                login({ ...user, ...data });
+                await storeObjectData('userLogged', { ...user, ...data });
+                Alert.alert('Sucesso', 'Dados atualizados com sucesso');
+                router.replace('/home');
+            } else {
+                const data = await response.json();
+                Alert.alert('Erro ao atualizar', data.error || 'Erro desconhecido');
+                console.log(data.error);
+            }
+        } catch (error) {
+            Alert.alert('Erro ao atualizar', 'Erro de rede ou servidor');
+            console.error('Erro ao atualizar:', error);
+        }
+    };
 
     return (
         <View style={styles.container}>
             <NavbarPadrao texto="Editar dados"/>
             <ScrollView style={styles.container2}>
                 <View style={styles.image}>
-                    <TouchableOpacity >
+                    <TouchableOpacity>
                         <Image
                             source={require('../../../assets/images/nophoto.jpg')}
                             style={styles.perfilImage}
@@ -29,45 +99,45 @@ export default function AtualizarDadosUser() {
                     <TextInput
                         style={styles.input}
                         placeholder="Nome"
-                        value=''
-                        onChangeText=''
+                        value={name}
+                        onChangeText={setName}
                     />
                     <View style={styles.row}>
                         <TextInputMask
                             type={'cpf'}
                             style={[styles.input, styles.cidadeEstado]}
                             placeholder="CPF"
-                            value=''
-                            onChangeText=''
+                            value={cpf}
+                            onChangeText={setCpf}
                         />
-                        <TextInputMask
+                        {/* <TextInputMask
                             type={'datetime'}
                             options={{ format: 'DD/MM/YYYY' }}
                             style={styles.input}
                             placeholder="Data de Nascimento"
-                            value=''
-                            onChangeText=''
-                        />
+                            value={nascimento}
+                            onChangeText={setNascimento}
+                        /> */}
                     </View>
                     <TextInput
                         style={styles.input}
                         placeholder="Email"
-                        value=''
-                        onChangeText=''
+                        value={email}
+                        onChangeText={setEmail}
                     />
 
                     <View style={styles.row}>
                         <TextInput
                             style={[styles.input, styles.cidadeEstado]}
                             placeholder="Cidade"
-                            value=''
-                            onChangeText=''
+                            value={cidade}
+                            onChangeText={setCidade}
                         />
                         <TextInput
                             style={[styles.input, styles.cidadeEstado]}
                             placeholder="Estado"
-                            value=''
-                            onChangeText=''
+                            value={estado}
+                            onChangeText={setEstado}
                         />
                     </View>
 
@@ -80,14 +150,13 @@ export default function AtualizarDadosUser() {
                         }}
                         style={styles.input}
                         placeholder="Telefone"
-                        value=''
-                        onChangeText=''
+                        value={telefone}
+                        onChangeText={setTelefone}
                         keyboardType='numeric'
                     />
-
                 </View>
             </ScrollView>
-            <Button>Confirmar</Button>
+            <Button onPress={handleUpdate}>Confirmar</Button>
         </View>
     );
 }
@@ -101,7 +170,7 @@ const styles = StyleSheet.create({
     container2: {
         flex: 1,
         padding: 20,
-        marginTop:30,
+        marginTop: 30,
         position: 'relative'
     },
     headerContainer: {
